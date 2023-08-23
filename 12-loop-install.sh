683d00f12f145417f50205de807064d2749178b4
sahiyaj <sahiyaj725@gmail.com>
2023-08-23 22:36:42 +0530

    message

--- 12-loop-install.sh
+++ 12-loop-install.sh
@@ -8,6 +8,7 @@ LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -31,7 +32,13 @@ fi
 
 for i in $@
 do
-    yum list installed $i
-    yum install $i -y &>>$LOGFILE
+    yum list installed $i &>>$LOGFILE 
+    if [ $? -ne 0 ]
+    then
+        echo  "$i is not installed, Lets install"
+        yum install -y &>>$LOGFILE
+    else
+        echo -e  "$Y $i is Already installed $N"    
+    
     VALIDATE $? "Installing $i"
-done    
+done
