38836471ad62951099c79b2c71fa2f600020cd42
sahiyaj <sahiyaj725@gmail.com>
2023-08-23 21:11:54 +0530

    message

--- 10-function-redirection.sh
+++ 10-function-redirection.sh
@@ -4,15 +4,19 @@ DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 ## this function should validate the previous command and inform user it is success 0r failure
 VALIDATE(){
     ## $1=arguement1=$?
     if [ $1 -ne 0 ]
     then 
-        echo "$2...FAILURE"
+        echo "$2...$R FAILURE $N"
         exit 1
     else
-        echo "$2...SUCCESS"
+        echo "$2...$G SUCCESS $N"
 
     fi 
 }
