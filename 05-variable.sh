79ea78ba21e82bbcef8be7fba9e6e82ccf5b6cae
sahiyaj <sahiyaj725@gmail.com>
2023-08-23 17:13:57 +0530

    message

--- 05-variable.sh
+++ 05-variable.sh
@@ -2,9 +2,8 @@
 
 echo "Please enter your username"
 read -s USERNAME
-echo "USERNAME entered is : $USERNAME"
+
 
 echo "Please enter the password"
 read -s PASSWORD
-echo "PASSWORD entered is : $PASSWORD"
 
