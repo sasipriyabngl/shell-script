ea98c3975b55b780d31cf84c1d97df358839c34e
sahiyaj <sahiyaj725@gmail.com>
2023-08-23 17:30:30 +0530

    message

---
+++ array.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# this is called array, instead of single value it can hold multiple values
+PERSONS=("saroja" "lohia" "sahiya")
+# inside array we always refer values with index and starts with 0(ZERO)
+
+echo "First person: ${PERSONS[0]}"
