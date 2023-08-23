8cad6f900b46fd948c44873ad5b98645368e1419
sahiyaj <sahiyaj725@gmail.com>
2023-08-23 21:49:40 +0530

    message

---
+++ 12-loop-install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DATE=$(date %F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2..$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2..$G SUCCESS $N"
+    fi
+}
+
+USERID=$(id -u)
+
+if [$USERID -ne 0 ]
+then
+    echo "ERROR:: Please run the script with root access"
+    exit 1
+else
+    echo "INFO:: yo are root user"
+fi    
+
+for i in $@
+do
+    yum install $i -y
+done    
