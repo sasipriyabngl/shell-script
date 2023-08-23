84e045611bc8d627be509a2e611b9a362c2c65a3
sahiyaj <sahiyaj725@gmail.com>
2023-08-23 21:13:50 +0530

    message

--- 10-function-redirection.sh
+++ 10-function-redirection.sh
@@ -29,8 +29,8 @@ then
     exit 1
 fi
 
-yum install mysql -y &>>$LOGFILE
-VALIDATE $? "Installing mysqll"
+yum install mysqll -y &>>$LOGFILE
+VALIDATE $? "Installing mysql"
 
 yum install postfix -y &>>$LOGFILE
 VALIDATE $? "Installing postfix"
