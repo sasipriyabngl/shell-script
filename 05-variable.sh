466ae0a329b1d23819b3a4b85c5a05801d881649
sahiyaj <sahiyaj725@gmail.com>
2023-08-23 17:15:18 +0530

    message

--- 05-variable.sh
+++ 05-variable.sh
@@ -2,8 +2,9 @@
 
 echo "Please enter your username"
 read -s USERNAME
-
+echo "USERNAME entered is : $USERNAME" # it will display the username on the terminal, if don't want to show in terninal delete the line
 
 echo "Please enter the password"
 read -s PASSWORD
+echo "PASSWORD entered is : $PASSWORD" # it will display the password on the terminal, if don't want to show in terninal delete the line
 
