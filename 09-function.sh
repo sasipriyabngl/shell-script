36033cd1d63789a3cf0b16965c57807077dc417d
sahiyaj <sahiyaj725@gmail.com>
2023-08-23 19:45:26 +0530

    message

---
+++ 09-function.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+## this function should validate the previous command and inform user it is success 0r failure
+VALIDATE(){
+    ## $1=arguement1=$?
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+
+    fi 
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+fi
+
+yum install mysql -y
+VALIDATE $? "Installing mysql"
+
+yum install postfx -y 
+VALIDATE $? "Installing postfix"
