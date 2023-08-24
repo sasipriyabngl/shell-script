ffc19605da70aa8f6eaab0fd26ee1778b08ada69
sahiyaj <sahiyaj725@gmail.com>
2023-08-24 12:57:17 +0530

    message

--- 13-delete-old-log.sh
+++ 13-delete-old-log.sh
@@ -10,3 +10,8 @@ LOGFILE=$LOGSDIR/$0-$DATE.log
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +10)
 
 echo "$FILES_TO_DELETE"
+
+while read line
+do
+    echo "Deleting $line" &>> $LOGFILE
+done <<< $FILES_TO_DELETE    
